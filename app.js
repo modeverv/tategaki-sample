ca879114a0ea936b4b3a3ab0a060333377208230
modeverv <modeverv@gmail.com>
2019-04-22 02:40:48 +0900

    a

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
+function load(){
+  $("#cc").html("Loading...");
+  var src = $("#src").val();
+  if(!src){
+    src = "https://kakuyomu.jp/works/1177354054881684388/episodes/1177354054881686249";
+  }
+  src = "https://cors.io/?" + src;
+  $.get(src)
+      .then(function(data){
+        var html_ar = [];
+        $data = $(data);
+        $data.find("#contentMain-inner").find("p").each(function(idx,html){
+          html_ar.push("<p>" + $(html).html() + "</p>");
+        });
+        $("#cc").html(html_ar.join(""));
+        var c = $(".widget-episode-inner");
+        c[0].scrollBy((40 * 2  * 9000000),0);
+        $("#close").click();
+      });
+}
+
 $(function(){
   var c = $(".widget-episode-inner");
+  var right = () => {
+     c[0].scrollBy((40 * 2  * 9),0);
+  };
+  var left = () => {
+     c[0].scrollBy(-(40 * 2 * 9),0);
+  };
+  $("#right").click(function(){right();});
+  $("#left").click(function(){left()});
   $(window).keyup(function(e){
-    window.console.log(e.keyCode);
+    // 進む
     if(e.keyCode == 37){
-      c[0].scrollBy(-800,0);
+      left();
     }
+    // 戻る
     if(e.keyCode == 39){
-      c[0].scrollBy(800,0);
+      right();
     }
   });
 });
