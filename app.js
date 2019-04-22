c44465c02db08fbe740e3bcca2d8af3702698678
modeverv <modeverv@gmail.com>
2019-04-22 23:04:56 +0900

    シンプルに

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function load(){
-  $("#cc").html("Loading...");
+  $("#c").html("Loading...");
   var src = $("#src").val();
   if(!src){
     src = "https://kakuyomu.jp/works/1177354054881684388/episodes/1177354054881686249";
@@ -8,27 +8,27 @@ function load(){
   $.get(src)
       .then(function(data){
         var html_ar = [];
-        $data = $(data);
-        $data.find("#contentMain-inner").find("p").each(function(idx,html){
+        $(data).find("#contentMain-inner").find("p").each(function(idx,html){
           html_ar.push("<p>" + $(html).html() + "</p>");
         });
-        $("#cc").html(html_ar.join(""));
-        var c = $(".widget-episode-inner");
-        c[0].scrollBy((40 * 2  * 9000000),0);
+        $("#c").html(html_ar.join(""));
+        var $c = $("#c");
+        $c[0].scrollBy((40 * 2 * 9000000),0);
+        $("#src").val("");
         $("#close").click();
       });
 }
 
 $(function(){
-  var c = $(".widget-episode-inner");
+  var $c = $("#c");
   var right = () => {
-     c[0].scrollBy((40 * 2  * 9),0);
+     $c[0].scrollBy((40 * 2 * 9),0);
   };
   var left = () => {
-     c[0].scrollBy(-(40 * 2 * 9),0);
+     $c[0].scrollBy(-(40 * 2 * 9),0);
   };
   $("#right").click(function(){right();});
-  $("#left").click(function(){left()});
+  $("#left").click(function(){left();});
   $(window).keyup(function(e){
     // 進む
     if(e.keyCode == 37){
